d49a009e67d35ca10afd89e76a9f855145b38896
Joshua Miles <joshmilesproduction@gmail.com>
2018-02-17 19:38:57 +0000

    added the silph road nest atlas

---
+++ DEXTER/DEXTER/ThirdViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ThirdViewController.swift
+//  DEXTER
+//
+//  Created by Joshua Miles on 17/02/2018.
+//  Copyright © 2018 Joshua Miles. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class ThirdViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.scrollView.isScrollEnabled = false
+        let url = URL(string: "https://thesilphroad.com/atlas")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
