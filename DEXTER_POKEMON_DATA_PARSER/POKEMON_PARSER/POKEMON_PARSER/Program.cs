103bbe80e773ecfb60420ab7e2f064c82e0905ac
Joshua Miles <joshmilesproduction@gmail.com>
2018-02-18 23:36:47 +0000

    Created pokemon data parser

---
+++ DEXTER_POKEMON_DATA_PARSER/POKEMON_PARSER/POKEMON_PARSER/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+
+namespace POKEMON_PARSER
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to begin!");
+            Console.ReadKey();
+            using (TextFieldParser parser = new TextFieldParser("convertcsv.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                int i = 0;
+                while (!parser.EndOfData)
+                {
+                    if (i != 0)
+                    {
+                        string[] fields = parser.ReadFields();
+                        Console.WriteLine(fields[1]);
+                        foreach (string field in fields)
+                        {
+                        }
+                    }
+                    i++;
+                }
+            }
+            Console.WriteLine("DONE!");
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Dexter
-Pokémon Go Pokédex &amp; Helper
+The Pokémon Go companion app!
+
+# TO DO
+- Android port
+- Add IV calculator
+- Icons next to names in Pokédex
+- Add updated data to the database
